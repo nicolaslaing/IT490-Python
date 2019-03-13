84387c4bb4baf9cbc680e474ceeaff762e6dfc1b
Nicolas Laing <nicolaslaing@outlook.com>
2019-03-12 23:57:03 -0400

    Added option for a different IP as a command-line arg, also converted publish to a POST, so an object can be sent through rabbitmq

--- app/app.py
+++ app/app.py
@@ -1,52 +1,71 @@
 #!/usr/bin/env python
 import pika
 import json
-from flask import Flask, render_template
+from flask import Flask, request, render_template
+import sys
 # import MySQLdb
 # import random
 
 app = Flask(__name__)
 
+ip = '192.168.2.174'
+if len(sys.argv) > 1:
+	ip = str(sys.argv[1])
+
 credentials = pika.PlainCredentials('NickLai','rmq12490')
-parameters = pika.ConnectionParameters('192.168.1.6', '5672', 'Theta', credentials)
+parameters = pika.ConnectionParameters(ip, '5672', 'Theta', credentials)
 connection = pika.BlockingConnection(parameters)
 channel = connection.channel()
 
-@app.route('/publish')
-def publish():
+##################################################### PUBLISH #####################################################
 
-	obj = {
-		'yeet': 'yuh'
-	}
+# You can run the following command to from a terminal send this publish request:
+# curl -X POST -H "Accept: application/json" -H "Content-Type: application/json" 
+# -d '{"key","value"}' 0.0.0.0:5000/publish/{name-of-queue}
 
+@app.route('/publish/<queue>', methods=['POST'])
+def publish(queue):
 
-	channel.queue_declare(queue='object')
+	content = request.get_json()
+	
+	channel.queue_declare(queue=queue)
 
 	channel.basic_publish(exchange='',
-                      routing_key='object',
-                      body=json.dumps(obj, ensure_ascii=False)) # <--- must be string or unicode, not object
+                      routing_key=queue,
+                      body=json.dumps(content, ensure_ascii=False))
 
 	print(" [x] Sent object")
 
-	connection.close()
-	# return render_template("index.html")
-	return "Hello"
+	# connection.close()
+	return str(200)
 
-@app.route('/consume')
-def consume():
+##################################################### CONSUME #####################################################
 
-	channel.queue_declare(queue='object')
+# You can run the following command from a terminal to send this consume request:
+# curl 0.0.0.0:5000/consume/{name-of-queue}
+
+# You sould see a print out of whatever data was sent via the publish route
+# in the python debug console which runs this file
+
+@app.route('/consume/<queue>', methods=['GET'])
+def consume(queue):
+
+	channel.queue_declare(queue=queue)
 
 	def callback(ch, method, properties, body):
 	    print(" [x] Received %r" % body)
 
 	channel.basic_consume(callback,
-	                      queue='object',
+	                      queue=queue,
 	                      no_ack=True)
+
+
+	# response = curl(url)
+	# post('/publish', response, 'response1')
 
 	print('Waiting for messages...')
 	channel.start_consuming()
-	return "Hello"
+	return str(200)
 
 if __name__ == '__main__':
 	app.run(host='0.0.0.0')
