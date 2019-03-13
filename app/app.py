c6e9493e6b0617d6e295ae32889e17dde1f2bbf6
Nicolas Laing <nicolaslaing@outlook.com>
2019-03-13 01:29:35 -0400

    Added code that might be useful in the DMZ script. Added Python HTTP package, and a test API route for SecondHandSongs

--- app/app.py
+++ app/app.py
@@ -2,20 +2,21 @@
 import pika
 import json
 from flask import Flask, request, render_template
+from flask.json import jsonify
+import requests
 import sys
 # import MySQLdb
 # import random
 
 app = Flask(__name__)
 
+queueBody=''
 ip = '192.168.2.174'
 if len(sys.argv) > 1:
 	ip = str(sys.argv[1])
 
 credentials = pika.PlainCredentials('NickLai','rmq12490')
 parameters = pika.ConnectionParameters(ip, '5672', 'Theta', credentials)
-connection = pika.BlockingConnection(parameters)
-channel = connection.channel()
 
 ##################################################### PUBLISH #####################################################
 
@@ -25,6 +26,9 @@ channel = connection.channel()
 
 @app.route('/publish/<queue>', methods=['POST'])
 def publish(queue):
+
+	connection = pika.BlockingConnection(parameters)
+	channel = connection.channel()
 
 	content = request.get_json()
 	
@@ -36,7 +40,9 @@ def publish(queue):
 
 	print(" [x] Sent object")
 
-	# connection.close()
+	channel.close()
+	connection.close()
+
 	return str(200)
 
 ##################################################### CONSUME #####################################################
@@ -50,22 +56,69 @@ def publish(queue):
 @app.route('/consume/<queue>', methods=['GET'])
 def consume(queue):
 
+	global queueBody
+	connection = pika.BlockingConnection(parameters)
+	channel = connection.channel()
+
 	channel.queue_declare(queue=queue)
 
 	def callback(ch, method, properties, body):
-	    print(" [x] Received %r" % body)
+		global queueBody
+		queueBody = json.loads(body) # convert string to json
+		print(" [x] Received %r" % body)
+		channel.stop_consuming()
+		channel.close()
+		connection.close()
 
 	channel.basic_consume(callback,
 	                      queue=queue,
 	                      no_ack=True)
 
-
-	# response = curl(url)
-	# post('/publish', response, 'response1')
-
 	print('Waiting for messages...')
+	
 	channel.start_consuming()
-	return str(200)
+
+	# Code for DMZ script; comment out if you're not using a DMZ server
+
+	# API data to be inserted into the API URL
+	# e.g queueBody = {"id": "123", "route": "performances"}
+	api_ID = queueBody["id"]
+	api_ROUTE = queueBody["route"]
+	URL = 'https://secondhandsongs.com/artist/' + api_ID + '/' + api_ROUTE + '?format=json'
+
+	res = requests.get(url=URL)
+	if res.ok:
+	    return jsonify(res.json())
+	else:
+		return "Internal server error (500) while accessing " + URL + "\n"
+	# - END DMZ SCRIPT -
+
+	return "HTTP Request Successful (200)\n"
 
 if __name__ == '__main__':
 	app.run(host='0.0.0.0')
+
+# Pika code that seemed interesting:
+
+	# # Get ten messages and break out
+	# for method_frame, properties, body in channel.consume(queue):
+
+	#     # Display the message parts
+	#     print method_frame
+	#     print properties
+	#     print body
+	#     queueBody = body
+	#     # Acknowledge the message
+	#     channel.basic_ack(method_frame.delivery_tag)
+
+	#     # Escape out of the loop after 10 messages
+	#     if method_frame.delivery_tag == 10:
+	#     	break
+
+	# # Cancel the consumer and return any pending messages
+	# requeued_messages = channel.cancel()
+	# print 'Requeued %i messages' % requeued_messages
+
+	# # Close the channel and the connection
+	# channel.close()
+	# connection.close()
